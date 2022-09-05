6a278401b8fc28ba24fb58858b0b9098055a7e3e
Ameen Shah <ameen.sha@trinityskillworks.com>
2022-09-05 22:49:33 +0530

    connected frontend and backend and made api call

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const dotenv = require("dotenv")
+const { chats } = require('./data/data.js');
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, function(){
-    console.log("Server Started at http://localhost:5000")
+app.get("/", (req, res) => {
+    res.send("API is running")
+})
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+})
+app.get("/api/chat/:id", (req, res) => {
+    // console.log(req.params.id)
+    const singleChat = chats.filter(chat => chat._id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, function(){
+    console.log(`Server Started at http://localhost:${PORT}`)
 })
